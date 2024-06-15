0bb7384635b50180e0c469e052a04dce5190dd20
aherbas3 <abigailrominaherbas@gmail.com>
2024-06-15 17:16:30 -0400

    finish controllers

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -44,12 +44,58 @@ const createWorkout = async (req, res) => {
     }
 }
 //delete a workout
+const deleteWorkout = async (req, res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        //if the id we pass in is not valid
+        //we need this bc otherwise if we give ids that aren't the right format, we'll have an internal error that will lead to crashes
+        return res.status(404).json({error: 'no such workout'})
+    }
+
+    //delete it. find the object whose _id property is same as our id const
+    // this const now holds the data of the workout we deleted
+    const workout = await Workout.findOneAndDelete({_id: id})
+
+    if (!workout) {
+        //if the id is valid but we cant find it
+        return res.status(400).json({error: 'no such workout'})
+    }
+
+    //else, we return the json of the workout we deleted
+    res.status(200).json(workout)
+}
 
 //update a workout
+const updateWorkout = async (req, res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        //if the id we pass in is not valid
+        //we need this bc otherwise if we give ids that aren't the right format, we'll have an internal error that will lead to crashes
+        return res.status(404).json({error: 'no such workout'})
+    }
+
+    //first arg is find criteria, second is an object representing update to make
+    //... is spreading all the properties of req.body into a new object
+    const workout = await Workout.findOneAndUpdate({_id: id}, {
+        ...req.body
+    })
+
+    if (!workout) {
+        //if the id is valid but we cant find it
+        return res.status(400).json({error: 'no such workout'})
+    }
+
+    //if found, send json of orig workout that was updated
+    res.status(200).json(workout)
+}
 
 
 module.exports = {
     getWorkouts,
     getWorkout,
-    createWorkout
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
 }
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -3,7 +3,9 @@ const Workout = require('../models/workoutModel')
 const {
     getWorkouts,
     getWorkout,
-    createWorkout
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
 } = require('../controllers/workoutController')
 
 //CREATE A ROUTER
@@ -20,14 +22,10 @@ router.get('/:id', getWorkout)
 router.post('/', createWorkout)
 
 //DELETE a workout
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'DELETE a workout'})
-})
+router.delete('/:id', deleteWorkout)
 
 //UPDATE a workout
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'UPDATE a workout'})
-})
+router.patch('/:id', updateWorkout)
 
 
 
